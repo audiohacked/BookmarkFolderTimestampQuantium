b325b234d515498f837d34460fa7afe43e5b0c30
Sean Nelson <audiohacked@gmail.com>
2018-05-20 13:20:53 -0600

    Update timestamp format to YYYY-MM-DD HH:mm:ss GMT+Offset

--- background.js
+++ background.js
@@ -1,5 +1,36 @@
 var currentBookmarkFolderId;
 
+if (!Date.prototype.BookmarkLocalTimestamp) {
+	(function() {
+		function pre_pad(number, length) {
+			var str = "" + number;
+			while (str.length < length) {
+				str = '0'+str;
+			}
+			return str;
+		}
+		function post_pad(number, length) {
+			var str = "" + number;
+			while (str.length < length) {
+				str = str+'0';
+			}
+			return str;
+		}
+		Date.prototype.BookmarkLocalTimestamp = function() {
+			var offset = this.getTimezoneOffset();
+			var hour_offset = pre_pad(offset/60, 2);
+			return this.getUTCFullYear() +
+				'-' + pre_pad(this.getUTCMonth() + 1, 2) +
+				'-' + pre_pad(this.getUTCDate(), 2) +
+				' ' + pre_pad(this.getHours(), 2) +
+				':' + pre_pad(this.getUTCMinutes(), 2) +
+				':' + pre_pad(this.getUTCSeconds(), 2) +
+				' GMT' + (offset<0 ? '+' : '-') + post_pad(hour_offset, 4);
+	
+		};
+	}());
+}
+
 /*
  *
  */
@@ -7,7 +38,7 @@ function timestampFolder(id, bookmarkInfo) {
     if (bookmarkInfo.type == "folder") {
         var myDate = new Date(0);
         myDate.setUTCSeconds(bookmarkInfo.dateAdded/1000);
-        browser.bookmarks.update(id, {title: myDate.toISOString()});
+        browser.bookmarks.update(id, {title: myDate.BookmarkLocalTimestamp()});
     }
 }
 
